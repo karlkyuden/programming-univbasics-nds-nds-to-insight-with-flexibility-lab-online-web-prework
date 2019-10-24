93ae668d0d6af1b2c9aac8eeac34ec3a73e26c66
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-24 03:49:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -71,7 +71,7 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   studio_gross_totals = {}
-
+  director_index = 0
   while director_index < collection.size do
     if !( studio_gross_totals.has_key? 'collection[director_index][:studio]' )
       studio_gross_totals[collection[director_index][:studio]] = 0
